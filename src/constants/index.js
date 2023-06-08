e9ed1c7464f4e8e8783d37a74314e93a7e9abd0e
codedbyshaun <shaun@codedbyshaun.com>
2023-06-08 18:25:46 +0200

    rename App.jsx, main.jsx + add constants files

---
+++ src/constants/index.js
@@ -0,0 +1,198 @@
+import { people01, people02, people03, facebook, instagram, linkedin, twitter, airbnb, binance, coinbase, dropbox, send, shield, star  } from "../assets";
+
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "features",
+    title: "Features",
+  },
+  {
+    id: "product",
+    title: "Product",
+  },
+  {
+    id: "clients",
+    title: "Clients",
+  },
+];
+
+export const features = [
+  {
+    id: "feature-1",
+    icon: star,
+    title: "Rewards",
+    content:
+      "Get rewarded for using PayFi. Every time you transact with us, earn points on your PayFi card.",
+  },
+  {
+    id: "feature-2",
+    icon: shield,
+    title: "100% Secured",
+    content:
+      "Feel confident knowing your transactions are protected by industry-standard BTB security protocols.",
+  },
+  {
+    id: "feature-3",
+    icon: send,
+    title: "Balance Transfer",
+    content:
+      "Effortlessly transfer your systems from your current provider, free of charge.",
+  },
+];
+
+export const feedback = [
+  {
+    id: "feedback-1",
+    content:
+      "PayFi has been a game-changer for my online store. I can accept payments from anywhere in the world, with low fees and fast transactions. And I love the rewards program!",
+    name: "Jessica",
+    title: "Boutique Owner",
+    img: people01,
+  },
+  {
+    id: "feedback-2",
+    content:
+      "PayFi has helped me manage my finances better. I can open a digital bank account in minutes, with no paperwork and no hidden charges. I can also access a variety of financial services, such as loans, investments, and savings.",
+    name: "Steven",
+    title: "Project Manager",
+    img: people02,
+  },
+  {
+    id: "feedback-3",
+    content:
+      "New opportunities have opened up for me. I can connect to the web3 ecosystem and explore the decentralized economy. I can buy and sell cryptocurrencies, NFTs, DeFi products, and more. I think PayFi is the future of finance.",
+    name: "Megan",
+    name: "Haven",
+    title: "Metaverse artist",
+    img: people03,
+  },
+];
+
+export const stats = [
+  {
+    id: "stats-1",
+    title: "Active Users",
+    value: "3800+",
+  },
+  {
+    id: "stats-2",
+    title: "Companies trust PayFi",
+    value: "230+",
+  },
+  {
+    id: "stats-3",
+    title: "Transactions to date",
+    value: "$230M+",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "Useful Links",
+    links: [
+      {
+        name: "Content",
+        link: "https://www.hoobank.com/content/",
+      },
+      {
+        name: "How it Works",
+        link: "https://www.hoobank.com/how-it-works/",
+      },
+      {
+        name: "Create",
+        link: "https://www.hoobank.com/create/",
+      },
+      {
+        name: "Explore",
+        link: "https://www.hoobank.com/explore/",
+      },
+      {
+        name: "Terms & Services",
+        link: "https://www.hoobank.com/terms-and-services/",
+      },
+    ],
+  },
+  {
+    title: "Community",
+    links: [
+      {
+        name: "Help Center",
+        link: "https://www.hoobank.com/help-center/",
+      },
+      {
+        name: "Partners",
+        link: "https://www.hoobank.com/partners/",
+      },
+      {
+        name: "Suggestions",
+        link: "https://www.hoobank.com/suggestions/",
+      },
+      {
+        name: "Blog",
+        link: "https://www.hoobank.com/blog/",
+      },
+      {
+        name: "Newsletters",
+        link: "https://www.hoobank.com/newsletters/",
+      },
+    ],
+  },
+  {
+    title: "Partners",
+    links: [
+      {
+        name: "Our Partners",
+        link: "https://www.hoobank.com/our-partner/",
+      },
+      {
+        name: "Become a Partner",
+        link: "https://www.hoobank.com/become-a-partner/",
+      },
+    ],
+  },
+];
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: instagram,
+    link: "https://www.instagram.com/",
+  },
+  {
+    id: "social-media-2",
+    icon: facebook,
+    link: "https://www.facebook.com/",
+  },
+  {
+    id: "social-media-3",
+    icon: twitter,
+    link: "https://www.twitter.com/",
+  },
+  {
+    id: "social-media-4",
+    icon: linkedin,
+    link: "https://www.linkedin.com/",
+  },
+];
+
+export const clients = [
+  {
+    id: "client-1",
+    logo: airbnb,
+  },
+  {
+    id: "client-2",
+    logo: binance,
+  },
+  {
+    id: "client-3",
+    logo: coinbase,
+  },
+  {
+    id: "client-4",
+    logo: dropbox,
+  },
+];
